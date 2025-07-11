9b38f597e1bf635320c21c0a53956029712d0deb
Amera <Amera.Abdelaziz@areebgroup.com>
2025-07-11 20:59:43 +0300

    edot quiz.routes.js

--- server/routes/quiz.routes.js
+++ server/routes/quiz.routes.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const quizController = require("../controllers/quiz.controller");
+const {createQuiz, getMyQuizzes, getQuizById, updateQuiz, deleteQuiz} = require("../controllers/quiz.controller");
 const { verifyTokenAndUserCheck } = require("../middleware/verifyToken");
 
 // All routes require authentication
 router.use(verifyTokenAndUserCheck);
 
 // Create a new quiz
-router.post("/", quizController.createQuiz);
+router.post("/", createQuiz);
 
 // Get all quizzes created by the logged-in teacher
-router.get("/my", quizController.getMyQuizzes);
+router.get("/my", getMyQuizzes);
 
 // Get a single quiz by ID
-router.get("/:id", quizController.getQuizById);
+router.get("/:id", getQuizById);
 
 // Update a quiz
-router.put("/:id", quizController.updateQuiz);
+router.put("/:id", updateQuiz);
 
 // Delete a quiz
-router.delete("/:id", quizController.deleteQuiz);
+router.delete("/:id", deleteQuiz);
 
 module.exports = router; 
