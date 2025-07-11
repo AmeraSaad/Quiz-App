93156c41755ea1fa1e5b4f9867e996873ef6a1ce
Amera <Amera.Abdelaziz@areebgroup.com>
2025-07-11 21:24:48 +0300

    add on quiz.routes.js file

--- server/routes/quiz.routes.js
+++ server/routes/quiz.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const quizController = require("../controllers/quiz.controller");
-const {createQuiz, getMyQuizzes, getQuizById, updateQuiz, deleteQuiz} = require("../controllers/quiz.controller");
+const {createQuiz, getMyQuizzes, getQuizById, updateQuiz, deleteQuiz, getAvailableQuizzes} = require("../controllers/quiz.controller");
 const { verifyTokenAndUserCheck } = require("../middleware/verifyToken");
+const { submitQuiz } = require("../controllers/submission.controller");
 
 // All routes require authentication
 router.use(verifyTokenAndUserCheck);
@@ -12,6 +13,9 @@ router.post("/", createQuiz);
 
 // Get all quizzes created by the logged-in teacher
 router.get("/my", getMyQuizzes);
+
+// Get all available quizzes for students
+router.get("/available", getAvailableQuizzes);
 
 // Get a single quiz by ID
 router.get("/:id", getQuizById);
